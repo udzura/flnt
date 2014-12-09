0467fa90a8e2b1e110457b056785ebe9aeadd6e9
Uchio KONDO <udzura@pepabo.com>
2014-12-09 19:46:35 +0900

    Add Flnt.test_mode?

--- lib/flnt.rb
+++ lib/flnt.rb
@@ -29,14 +29,26 @@ module Flnt
     end
 
     def initialize!
-      unless initialized?
+      if !initialized? and !test_mode?
         @initialized = true
         Fluent::Logger::FluentLogger.open(*Flnt::Configuration())
       end
     end
 
     def tag!(tag)
+      initialize!
       Flnt::Logger.new(tag.to_s)
+    end
+
+    # This is useful for testing
+    def test_mode!
+      @test_mode = true
+      # Switch the default logger
+      ::Fluent::Logger.open(::Fluent::Logger::TestLogger)
+    end
+
+    def test_mode?
+      @test_mode || false
     end
   end
 end
